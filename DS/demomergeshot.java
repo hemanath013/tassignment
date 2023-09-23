767e5c7dc314abfd131100bb03bbe9276d470165
hemanath013 <hemanathsd@gmail.com>
2023-09-23 15:12:37 +0530

    merge sort

---
+++ DS/demomergeshot.java
@@ -0,0 +1,8 @@
+public class demomergesort {
+    public static void main (String [] args){
+    int arr [] = {23,13,25,65,76,0013};
+    
+
+    }
+    
+}
---
+++ DS/demomergesort.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class demomergesort {
+    public static void main (String [] args){
+    int arr [] = {23,13,25,65,76,0013};
+    mergeshot.mergeshot(arr, 0, 5);
+    System.out.println(Arrays.toString(arr));
+
+    }
+    
+}
